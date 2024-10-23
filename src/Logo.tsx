1099a1c249c421487ad5b922b1aeb3b0efd427b9
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-23 13:30:43 +0200

    Added a logo

---
+++ src/Logo.tsx
@@ -0,0 +1,12 @@
+import timer_logo from './assets/timer_logo.png'
+
+function Logo(){
+
+    <h1>
+        Welcome to the Timer
+        <img src={timer_logo} className="timer_logo" alt="timer_logo" />
+    </h1>
+    
+}
+
+export default Logo
