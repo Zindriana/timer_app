cd4e30cff7f4b2c65f21afc16ef0adc0f8e8d28d
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-23 17:01:57 +0200

    added base

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/timer_app">
       <App />
     </BrowserRouter>
   </StrictMode>,
