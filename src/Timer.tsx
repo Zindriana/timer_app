8f393b2ade8c1f148596bf80d66a7c490baf1793
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-24 17:59:45 +0200

    added timer buttons

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,9 +1,34 @@
+import { useRef, useState } from "react";
 function Timer(){
 
+    const [time, setTime] = useState(0);
+    const intervalRef = useRef(0);
+
+    function startTimer(){
+        if(intervalRef.current === 0){
+            intervalRef.current = setInterval(increaseTimeCounterByOne, 1000);
+        }
+    }
+
+    function stopTimer(){
+        clearInterval(intervalRef.current);
+        intervalRef.current = 0;
+    }
+    
+    function increaseTimeCounterByOne(){
+        setTime(prevTime => prevTime+1);
+    }
+
     return(
-    <p>
-        Place holder Timer-sidan
-    </p>
+    <>
+        <p>Tid: {time} sekunder</p>
+        <button className="timer_btn" onClick={startTimer}>
+            Start timer
+        </button>
+        <button className="stop_btn" onClick={stopTimer}>
+            Stop timer
+        </button>
+    </>
     )
 }
 
