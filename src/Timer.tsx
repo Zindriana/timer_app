ac2a4ea8fcdb7a8e236e1e836a25a7f9c3c29297
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-24 19:15:50 +0200

    added an input for beepNumber

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -4,6 +4,7 @@ function Timer(){
 
     const [time, setTime] = useState(0);
     const intervalRef = useRef(0);
+    const [beepNumber, setBeepNumber] = useState(0);
 
     function startTimer(){
         if(intervalRef.current === 0){
@@ -20,12 +21,19 @@ function Timer(){
 
         setTime(prevTime => {
             const newTime = prevTime+1;
-            if (newTime % 5 === 0){
+            if (newTime % beepNumber === 0){
                 const audio = new Audio(mm_vaniljsas);
                 audio.play();
             }
             return newTime;
         });
+    }
+
+    function handleBeepChange(event: React.ChangeEvent<HTMLInputElement>){
+        if(Number(event)>0){
+            setBeepNumber(Number(event.target.value));
+        }
+        
     }
 
     return(
@@ -37,6 +45,7 @@ function Timer(){
         <button className="stop_btn" onClick={stopTimer}>
             Stop timer
         </button>
+        <input type='number' className="beepNumber" onChange={handleBeepChange}></input>
     </>
     )
 }
